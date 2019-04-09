981922c6dbf70cd494fb72ad5479c1bbc2958b04
Vonn <shevonnaboyd@gmail.com>
2019-04-08 19:57:20 -0700

    adding js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # unit-4-game
+
+Click on the crystals to make your added total score the same as the computers choice.
+
+Match the color and win!
+
+Go over teh cumputers number and lose!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,86 @@
-function randy() {
-    Return Math.floor(Math.random() * 101) +20
-    }
+$( document ).ready(function(){
+    var Randy=Math.floor(Math.random()*100+20)
+    $("#computer").text(Randy);
 
+var userTotal= 0; 
+var wins= 0;
+var loses= 0;
+var c1= Math.floor(Math.random()*11+1)
+var c2= Math.floor(Math.random()*11+1)
+var c3= Math.floor(Math.random()*11+1)
+var c4= Math.floor(Math.random()*11+1)
+   
+$("#winns").text(wins);
+$("#losses").text(loses);
+  
+  function winner(){
+  alert("You won!");
+    wins++; 
+    $("#winns").text(wins);
+    reset();
+  }
+ 
+  function loser(){
+  alert ("You lose!");
+    loses++;
+    $('#losses').text(loses);
+    reset()
+  }
 
-function gemmy(num) {
-    Return Math.floor(Math.random() * 12) +1
-    }
+function reset(){
+    Randy=Math.floor(Math.random()*100+20)
+    $('#computer').text(Randy);
+    // var elems = document.getElementsByTagName( "input" );
+    // var arr = jQuery.makeArray( input );
+        c1= Math.floor(Math.random()*11+1);
+        c2= Math.floor(Math.random()*11+1);
+        c3= Math.floor(Math.random()*11+1);
+        c4= Math.floor(Math.random()*11+1);
+        userTotal= 0;
+        $('#userAdd').text(userTotal);
+        } 
+ 
+    $('#c1').on ('click', function(){
+      userTotal = userTotal + c1;
+      $('#userAdd').text(userTotal);           
+          if (userTotal == Randy){
+            winner();
+          }
+          else if ( userTotal > Randy){
+            loser();
+          }   
+    })  
 
-    var c1 = gemmy();
-    var c2 = gemmy();
-    var c3 = gemmy();
-    var c4 = gemmy(),
+    $('#c2').on ('click', function(){
+        userTotal = userTotal + c2;
+        $('#userAdd').text(userTotal); 
+            if (userTotal == Randy){
+              winner();
+            }
+            else if ( userTotal > Randy){
+              loser();
+            }   
+      })    
 
-    // $('.login-button').on('click', () => {
+      $('#c3').on ('click', function(){
+        userTotal = userTotal + c3;
+        $('#userAdd').text(userTotal); 
+            if (userTotal == Randy){
+              winner();
+            }
+            else if ( userTotal > Randy){
+              loser();
+            }   
+      }) 
 
-//when
+      $('#c4').on ('click', function(){
+        userTotal = userTotal + c4;
+        $('#userAdd').text(userTotal); 
+            if (userTotal == Randy){
+              winner();
+            }
+            else if ( userTotal > Randy){
+              loser();
+            }   
+      }) 
+  }); 
