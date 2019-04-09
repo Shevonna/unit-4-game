ac4bf85fef075379953d83f88db5413ed21bfe41
Vonn <shevonnaboyd@gmail.com>
2019-04-08 18:09:41 -0700

    finshed css

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+function randy() {
+    Return Math.floor(Math.random() * 101) +20
+    }
+
+
+function gemmy(num) {
+    Return Math.floor(Math.random() * 12) +1
+    }
+
+    var c1 = gemmy();
+    var c2 = gemmy();
+    var c3 = gemmy();
+    var c4 = gemmy(),
+
+    // $('.login-button').on('click', () => {
+
+//when
